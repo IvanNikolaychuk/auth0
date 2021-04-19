b810bd3eb48e52f33655694b09c9339a7c739bcd
Ivan Nikolaichuk <dev@pipalia.co.uk>
2021-04-19 14:13:17 +0200

    - Working solution

--- src/main/java/com/example/springbootoauth2/AppController.java
+++ src/main/java/com/example/springbootoauth2/AppController.java
@@ -3,11 +3,8 @@ package com.example.springbootoauth2;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.jwt.Jwt;
-import org.springframework.security.jwt.JwtHelper;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,8 +15,8 @@ import java.util.Map;
 @RestController
 public class AppController {
 
-    @Value("${security.oauth2.resource.id}")
-    private String resourceId;
+    @Value("${auth0.audience}")
+    private String audience;
 
     @Value("${auth0.domain}")
     private String domain;
@@ -62,19 +59,27 @@ public class AppController {
         return new JSONObject()
                 .put("domain", domain)
                 .put("clientID", clientId)
-                .put("audience", resourceId)
+                .put("audience", audience)
                 .toString();
     }
 
     public String extractEmailFromToken() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object details = authentication.getDetails();
-        if ( details instanceof OAuth2AuthenticationDetails){
-            Jwt decodedToken = JwtHelper.decode(((OAuth2AuthenticationDetails) details).getTokenValue());
-            decodedToken.getClaims();
-            System.out.println("");
+        if (authentication.getPrincipal() instanceof Jwt) {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            Map<String, Object> claims = jwt.getClaims();
+            if (claims.containsKey("https://user.data.com/email")) {
+                return (String) claims.get("https://user.data.com/email");
+            } else {
+                throw new IllegalArgumentException("User has no email.");
+            }
         }
-
+//        if ( details instanceof OAuth2AuthenticationDetails){
+//            Jwt decodedToken = JwtHelper.decode(((OAuth2AuthenticationDetails) details).getTokenValue());
+//            decodedToken.getClaims();
+//            System.out.println("");
+//        }
+//
         return "";
     }
 }
--- src/main/java/com/example/springbootoauth2/SecurityConfig.java
+++ src/main/java/com/example/springbootoauth2/SecurityConfig.java
@@ -1,77 +1,47 @@
 package com.example.springbootoauth2;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 import java.util.Map;
-
 @Configuration
-@EnableResourceServer
-public class SecurityConfig extends ResourceServerConfigurerAdapter {
-    @Value("${security.oauth2.resource.id}")
-    private String resourceId;
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/api/public").permitAll()
-                .antMatchers("/api/admin").access("#oauth2.hasScope('admin:all')")
-                .antMatchers("/api/user").access("#oauth2.hasScope('user:all')")
+                .antMatchers("/api/admin").hasAuthority("SCOPE_admin:all")
+                .antMatchers("/api/user").hasAuthority("SCOPE_user:all")
                 .mvcMatchers("/api/**").authenticated()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and().oauth2ResourceServer().jwt();
     }
 
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+                JwtDecoders.fromOidcIssuerLocation(issuer);
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(resourceId);
-//        resources.tokenServices(createTokenServices());
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
     }
-
-//    @Bean
-//    public DefaultTokenServices createTokenServices() {
-//        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-//        defaultTokenServices.setTokenStore( createTokenStore() );
-//        return defaultTokenServices;
-//    }
-//
-//    @Bean
-//    public TokenStore createTokenStore() {
-//        return new JwtTokenStore( createJwtAccessTokenConverter() );
-//    }
-//
-//    @Bean
-//    public JwtAccessTokenConverter createJwtAccessTokenConverter() {
-//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setAccessTokenConverter( new JwtConverter() );
-//        return converter;
-//    }
-//
-//    public static class JwtConverter extends DefaultAccessTokenConverter implements JwtAccessTokenConverterConfigurer {
-//
-//        @Override
-//        public void configure(JwtAccessTokenConverter converter) {
-//            converter.setAccessTokenConverter(this);
-//        }
-//
-//        @Override
-//        public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
-//            OAuth2Authentication auth = super.extractAuthentication(map);
-//            auth.setDetails(map); //this will get spring to copy JWT content into Authentication
-//            return auth;
-//        }
-//    }
 }
 
